edae6b23f1c022afce92fefd4a4cf77f393cd80b
VERMA <ritika.b.verma@capgemini.com>
2018-06-18 17:39:03 +0530

    entity done

--- customer/src/main/java/com/banking/customer/entity/Address.java
+++ customer/src/main/java/com/banking/customer/entity/Address.java
@@ -1,12 +1,13 @@
 package com.banking.customer.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 /**
- * @author tuhindas
+ * @author 
  *
  */
 @Entity
@@ -14,8 +15,76 @@ public class Address {
 
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="Address_Id")
 	private Integer id;
 	private String street;
 	private String zipCode;
 	private String city;
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+	/**
+	 * 
+	 */
+	public Address() {
+		super();
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	/**
+	 * @return the street
+	 */
+	public String getStreet() {
+		return street;
+	}
+	/**
+	 * @param street the street to set
+	 */
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	/**
+	 * @return the zipCode
+	 */
+	public String getZipCode() {
+		return zipCode;
+	}
+	/**
+	 * @param zipCode the zipCode to set
+	 */
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+	/**
+	 * @return the city
+	 */
+	public String getCity() {
+		return city;
+	}
+	/**
+	 * @param city the city to set
+	 */
+	public void setCity(String city) {
+		this.city = city;
+	}
+	/**
+	 * @param id
+	 * @param street
+	 * @param zipCode
+	 * @param city
+	 */
+	public Address(Integer id, String street, String zipCode, String city) {
+		super();
+		this.id = id;
+		this.street = street;
+		this.zipCode = zipCode;
+		this.city = city;
+	}
 }
--- customer/src/main/java/com/banking/customer/entity/Customer.java
+++ customer/src/main/java/com/banking/customer/entity/Customer.java
@@ -1,13 +1,17 @@
 
 package com.banking.customer.entity;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 
 /**
- * @author tuhindas
+ * @author 
  *
  */
 @Entity
@@ -19,5 +23,86 @@ public class Customer {
 	private String firstName;
 	private String lastName;
 		
+	
+    @OneToOne(fetch=FetchType.EAGER,cascade = CascadeType.ALL)
+    @JoinColumn(name = "Address_Id", unique = true)
+	Address address;
+
+	/**
+	 * 
+	 */
+	public Customer() {
+		super();
+	}
+
+	/**
+	 * @param id
+	 * @param firstName
+	 * @param lastName
+	 * @param address
+	 */
+	public Customer(Integer id, String firstName, String lastName, Address address) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	/**
+	 * @return the address
+	 */
+	public Address getAddress() {
+		return address;
+	}
+
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(Address address) {
+		this.address = address;
+	}
 	
 }
